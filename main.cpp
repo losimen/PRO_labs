0f86b41fd6b12fb8ff4205ff5668d8691513f6cf
losimen <koval.pavlo03@gmail.com>
2023-04-05 23:02:59 +0300

    final version

--- main.cpp
+++ main.cpp
@@ -1,116 +1,145 @@
-#include <vector>
 #include <random>
 #include <iostream>
+#include <array>
+#include <iomanip>
 
+constexpr unsigned SIZE_N = 4;
 
-typedef std::vector<std::vector<double>> Matrix;
-typedef std::vector<double> Vector;
-
-
-namespace MatrixUtils
+template <unsigned N>
+class Matrix
 {
-    Matrix Transpose(const Matrix &matrix)
+private:
+    typedef std::array<std::array<double, N>, N> TMatrix;
+    typedef std::array<double, N> TVector;
+    TMatrix m_matrix;
+
+public:
+    Matrix() = delete;
+    Matrix(unsigned rows, unsigned cols): rows(rows), cols(cols)
     {
-        Matrix result;
-        result.reserve(matrix.size());
+        m_matrix.fill(TVector());
+    }
 
-        for (unsigned i = 0; i < matrix.size(); ++i)
-        {
-            Vector row;
-            row.reserve(matrix.size());
+    TVector& operator[](unsigned index) {
+        return m_matrix[index];
+    }
 
-            for (const auto & j : matrix)
-            {
-                row.emplace_back(j[i]);
-            }
+    unsigned rows;
+    unsigned cols;
 
-            result.emplace_back(row);
-        }
+    unsigned size() const
+    {
+        return m_matrix.size();
+    }
+
+    Matrix transpose()
+    {
+        Matrix<SIZE_N> result(rows, cols);
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+                result[i][j] = m_matrix[j][i];
 
         return result;
     }
 
-    Matrix Multiply(const Matrix &matrix1, const Matrix &matrix2)
+    Matrix mul(Matrix &oth)
     {
-        Matrix result;
-        result.reserve(matrix1.size());
+        if (m_matrix.size() != oth[0].size())
+            throw std::invalid_argument("m_matrix sizes are not equal");
 
-        for (const auto & i : matrix1)
+        Matrix<SIZE_N> result(m_matrix.size(), oth[0].size());
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
         {
-            Vector row;
-            row.reserve(i.size());
-
-            for (unsigned j = 0; j < i.size(); ++j)
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
             {
                 double sum = 0;
-                for (unsigned k = 0; k < i.size(); ++k)
+                for (unsigned k = 0; k < m_matrix[i].size(); ++k)
                 {
-                    sum += i[k] * matrix2[k][j];
+                    sum += m_matrix[i][k] * oth[k][j];
                 }
 
-                row.emplace_back(sum);
+                result[i][j] = sum;
             }
-
-            result.emplace_back(row);
         }
 
         return result;
     }
 
-    Matrix Sub(const Matrix &matrix1, const Matrix &matrix2)
+    Matrix sub(Matrix &oth)
     {
-        Matrix result;
-        result.reserve(matrix1.size());
+        if (m_matrix.size() != oth.size())
+            throw std::invalid_argument("m_matrix sizes are not equal");
 
-        for (unsigned i = 0; i < matrix1.size(); ++i)
+        Matrix<SIZE_N> result(m_matrix.size(), oth[0].size());
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
         {
-            Vector row;
-            row.reserve(matrix1[i].size());
-
-            for (unsigned j = 0; j < matrix1[i].size(); ++j)
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
             {
-                row.emplace_back(matrix1[i][j] - matrix2[i][j]);
+                result[i][j] = m_matrix[i][j] - oth[i][j];
             }
-
-            result.emplace_back(row);
         }
 
         return result;
     }
-}
 
-
-void printMatrix(const Matrix &matrix)
-{
-    for (const auto &row: matrix)
+    Matrix add(Matrix &oth)
     {
-        for (const auto &elem: row)
+        if (m_matrix.size() != oth.size())
+            throw std::invalid_argument("m_matrix sizes are not equal");
+
+        Matrix<SIZE_N> result(m_matrix.size(), oth[0].size());
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
         {
-            std::cout << elem << " ";
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+            {
+                result[i][j] = m_matrix[i][j] + oth[i][j];
+            }
         }
-        std::cout << std::endl;
+
+        return result;
     }
-}
 
+    void printMatrix()
+    {
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
+        {
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+            {
+                std::cout << std::setw(10) << std::setprecision(5) << m_matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+};
 
-void randGen(Matrix &A, int N, int start = 1, int end = 100)
+void randGenMatrix(Matrix<SIZE_N> &A, int N, int start = 1, int end = 100)
 {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(start, end);
 
-    A.reserve(N);
     for (unsigned i = 0; i < N; ++i)
     {
-        Vector row;
-        row.reserve(N);
-
         for (unsigned j = 0; j < N; ++j)
         {
-            row.emplace_back((int)dist(mt));
+            A[i][j] = dist(mt);
         }
+    }
+}
 
-        A.emplace_back(row);
+void randGenColumnVector(Matrix<SIZE_N> &A, int N, int start = 1, int end = 100)
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(start, end);
+
+    for (unsigned i = 0; i < N; ++i)
+    {
+        A[i][0] = dist(mt);
     }
 }
 
@@ -119,26 +148,28 @@ class Phase1
 {
 public:
     const int N;
-    Vector vectorColumnBi {};
-    Matrix A {};
+    Matrix<SIZE_N> vectorColumnBi = Matrix<SIZE_N>(N, 1);
+    Matrix<SIZE_N> A = Matrix<SIZE_N>(N, N);
 
     explicit Phase1(int n)
             : N(n)
-    {}
-
-    Vector countY1()
     {
-        Vector result;
-        result.reserve(vectorColumnBi.size());
+        genBi(9);
+        genA();
+    }
 
-        for (const auto &row: A)
+    Matrix<SIZE_N> countY1()
+    {
+        Matrix<SIZE_N> result(N, 1);
+
+        for (unsigned i = 0; i < N; ++i)
         {
             double sum = 0;
-            for (unsigned i = 0; i < row.size(); ++i)
+            for (unsigned j = 0; j < N; ++j)
             {
-                sum += row[i] * vectorColumnBi[i];
+                sum += A[i][j] * vectorColumnBi[i][0];
             }
-            result.emplace_back(sum);
+            result[i][0] = sum;
         }
 
         return result;
@@ -146,17 +177,15 @@ public:
 
     void genBi(int num)
     {
-        vectorColumnBi.reserve(N);
-
         for (unsigned i = 0; i < N; ++i)
         {
-            vectorColumnBi.emplace_back(int(num * (i + 1)));
+            vectorColumnBi[i][0] = int(num * (i + 1));
         }
     }
 
-    void genA()
+    void genA(int start = 0, int end = 100)
     {
-        randGen(A, N);
+        randGenMatrix(A, N, start, end);
     }
 };
 
@@ -165,69 +194,39 @@ class Phase2
 {
 public:
     const int N;
-    Matrix A1;
-    Vector vector_column_bi;
-    Vector vector_column_ci;
+    Matrix<SIZE_N> A1 = Matrix<SIZE_N>(N, N);
+    Matrix<SIZE_N> vectorColumnBi = Matrix<SIZE_N>(N, 1);
+    Matrix<SIZE_N> vectorColumnCi = Matrix<SIZE_N>(N, 1);
 
-    explicit Phase2(int n)
+    explicit Phase2(int n, int start = 1, int end = 100)
             : N(n)
-    {}
-
-    void genBi(int num)
     {
-        vector_column_bi.reserve(N);
-
-        for (unsigned i = 0; i < N; ++i)
-        {
-            vector_column_bi.emplace_back(int(num * (i + 1)));
-        }
+        genBi(start, end);
+        genCi(start, end);
+        genA1();
     }
 
-    void genCi(int num)
+    void genBi(int start, int end)
     {
-        vector_column_ci.reserve(N);
+        randGenColumnVector(vectorColumnBi, N, start, end);
+    }
 
-        for (unsigned i = 0; i < N; ++i)
-        {
-            vector_column_ci.emplace_back(int(num * (i + 1)));
-        }
+    void genCi(int start, int end)
+    {
+        randGenColumnVector(vectorColumnCi, N, start, end);
     }
 
     void genA1()
     {
-        randGen(A1, N);
+        randGenMatrix(A1, N);
     }
 
-    Vector countY2()
+    Matrix<SIZE_N> countY2()
     {
-        auto subTwoVectors = [](const Vector &v1, const Vector &v2)
-        {
-            std::vector<int> result;
-            result.reserve(v1.size());
+        Matrix<SIZE_N> result(N, 1);
 
-            for (unsigned i = 0; i < v1.size(); ++i)
-            {
-                result.emplace_back(v1[i] - v2[i]);
-            }
-
-            return result;
-        };
-
-        Vector result;
-        result.reserve(vector_column_ci.size());
-
-        for (const auto &row: A1)
-        {
-            const auto sub = subTwoVectors(row, vector_column_bi);
-
-            double sum = 0;
-            for (unsigned i = 0; i < sub.size(); ++i)
-            {
-                sum += sub[i] * vector_column_ci[i];
-            }
-
-            result.emplace_back(sum);
-        }
+        result = vectorColumnBi.sub(vectorColumnCi);
+        result = A1.mul(result);
 
         return result;
     }
@@ -238,74 +237,61 @@ class Phase3
 {
 public:
     const int N;
-    Matrix A2;
-    Matrix B2;
-    Matrix C2;
+    Matrix<SIZE_N> A2 = Matrix<SIZE_N>(N, N);
+    Matrix<SIZE_N> B2 = Matrix<SIZE_N>(N, N);
+    Matrix<SIZE_N> C2 = Matrix<SIZE_N>(N, N);
 
-    explicit Phase3(int n)
+    explicit Phase3(int n, int start = 1, int end = 100)
             : N(n)
-    {}
+    {
+        genA2(start, end);
+        genB2(start, end);
+        genC2();
+    }
 
     void genA2(int start = 1, int end = 100)
     {
-        randGen(A2, N, start, end);
+        randGenMatrix(A2, N, start, end);
     }
 
     void genB2(int start = 1, int end = 100)
     {
-        randGen(B2, N, start, end);
+        randGenMatrix(B2, N, start, end);
     }
 
     void genC2()
     {
-        C2.reserve(N);
         for (int i = 0; i < N; ++i)
         {
-            C2.reserve(N);
-            C2.emplace_back();
             for (int j = 0; j < N; ++j)
             {
-                C2[i].emplace_back(1 / (double)(i + 2*j+1));
+                C2[i][j] = 1 / (double)(i + j + 1);
             }
         }
-
-        std::cout << "C2: " << C2.size() << std::endl;
     }
 
-    Matrix countY3() const
+    Matrix<SIZE_N> countY3()
     {
-        // A2 * (B2 - C2)
-        const auto sub = MatrixUtils::Sub(B2, C2);
-        return MatrixUtils::Multiply(A2, sub);
+        Matrix<SIZE_N> sub = B2.add(C2);
+        return A2.mul(sub);
     }
 };
 
 
 int main()
 {
-    const int N = 4;
+    Phase1 phase1(4);
+    Matrix<SIZE_N> Y1 = phase1.countY1();
+    Matrix<SIZE_N> Y1_T = Y1.transpose();
 
-    Phase1 p1(N);
-    p1.genA();
-    p1.genBi(9);
-    Vector y1 = p1.countY1();
+    Phase2 phase2(4, 1, 100);
+    Matrix<SIZE_N> Y2 = phase2.countY2();
+    Matrix<SIZE_N> Y2_T = Y2.transpose();
 
-    Matrix y1_transposed = MatrixUtils::Transpose({y1});
-    printMatrix(y1_transposed);
-
-    Phase2 p2(N);
-    p2.genA1();
-    p2.genBi(9);
-    p2.genCi(7);
-    Vector y2 = p2.countY2();
-
-    Phase3 p3(N);
-    p3.genA2(1, 15);
-    p3.genB2(1, 15);
-    p3.genC2();
-    Matrix Y3 = p3.countY3();
-    Matrix Y3_2 = MatrixUtils::Multiply(Y3, Y3);
-    Matrix Y3_3 = MatrixUtils::Multiply(Y3_2, Y3);
+    Phase3 phase3(4, 1, 100);
+    Matrix<SIZE_N> Y3 = phase3.countY3();
+    Matrix<SIZE_N> Y3_2 = Y3.mul(Y3);
+    Matrix<SIZE_N> Y3_3 = Y3_2.mul(Y3);
 
     return 0;
 }
