71d739eddc7de7ca14feebbe68b11dc725e789f9
losimen <koval.pavlo03@gmail.com>
2023-04-22 19:35:11 +0300

    gen matrix a

--- main.cpp
+++ main.cpp
@@ -1,34 +1,206 @@
+#include <random>
 #include <iostream>
 #include <array>
+#include <iomanip>
+#include <map>
+#include <fstream>
 
-constexpr int SIZE_N = 6;
-typedef std::array<std::array<long double, SIZE_N>, SIZE_N> Matrix;
+constexpr unsigned SIZE_N = 6;
+
+typedef long double CalVar;
+typedef std::array<std::array<CalVar, SIZE_N>, SIZE_N> TMatrix;
+typedef std::array<CalVar, SIZE_N> TVector;
 
 
-Matrix genMatrixA()
+template <unsigned N>
+class Matrix
 {
-    // 111…111
-    //222…220
-    //333…300
-    //…
-    //n000…00
+private:
+    TMatrix m_matrix{};
 
-    Matrix A;
-    for (int i = 0; i < SIZE_N; ++i)
+    Matrix mul(Matrix &oth)
     {
-        for (int j = 0; j < SIZE_N; ++j)
+        if (this->cols != oth.rows)
+            throw std::invalid_argument("m_matrix sizes are not equal");
+
+        Matrix<SIZE_N> result(this->rows, oth.cols);
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < oth.cols; j++) {
+                for (size_t k = 0; k < cols; k++) {
+                    result[i][j] += m_matrix[i][k] * oth.m_matrix[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    Matrix mul(CalVar oth)
+    {
+        Matrix<SIZE_N> result(this->rows, this->cols);
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                result[i][j] = m_matrix[i][j] * oth;
+            }
+        }
+
+        return result;
+    }
+
+    Matrix sub(Matrix &oth)
+    {
+        if (this->rows != oth.rows && this->cols != oth.cols)
+            throw std::invalid_argument("m_matrix sizes are not equal");
+
+        Matrix<SIZE_N> result(std::max(this->rows, oth.rows), std::max(this->cols, oth.cols));
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
         {
-            A[i][j] = i * 10 + j;
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+            {
+                result[i][j] = m_matrix[i][j] - oth[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    Matrix add(Matrix &oth)
+    {
+        if (this->rows != oth.rows && this->cols != oth.cols)
+            throw std::invalid_argument("m_matrix sizes are not equal");
+
+        Matrix<SIZE_N> result(std::max(this->rows, oth.rows), std::max(this->cols, oth.cols));
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
+        {
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+            {
+                result[i][j] = m_matrix[i][j] + oth[i][j];
+            }
+        }
+
+        return result;
+    }
+public:
+    Matrix() = delete;
+    Matrix(unsigned rows, unsigned cols): rows(rows), cols(cols)
+    {
+        m_matrix.fill(TVector());
+    }
+
+    TVector& operator[](unsigned index) {
+        return m_matrix[index];
+    }
+
+    unsigned rows;
+    unsigned cols;
+
+    Matrix transpose()
+    {
+        Matrix<SIZE_N> result(this->cols, this->rows);
+
+        for (unsigned i = 0; i < m_matrix.size(); ++i)
+            for (unsigned j = 0; j < m_matrix[i].size(); ++j)
+                result[i][j] = m_matrix[j][i];
+
+        return result;
+    }
+
+    template<typename T>
+    Matrix operator*(T &&oth)
+    {
+        return mul(oth);
+    }
+
+    template<typename T>
+    Matrix operator-(T &&oth)
+    {
+        return sub(oth);
+    }
+
+    template<typename T>
+    Matrix operator+(T &&oth)
+    {
+        return add(oth);
+    }
+
+    void print()
+    {
+        std::cout << std::fixed;
+        for (auto & i : m_matrix)
+        {
+            for (long double j : i)
+            {
+                std::cout << std::setw(12) << std::setprecision(0) << j << " ";
+            }
+            std::cout << std::endl;
         }
     }
 
-    return A;
+    void writeToFile(const std::string &fileName)
+    {
+        std::ofstream file(fileName);
+        file << std::fixed;
+        for (auto & i : m_matrix)
+        {
+            for (long double j : i)
+            {
+                file << j << " ";
+            }
+            file << std::endl;
+        }
+    }
+
+    void readFromFile(const std::string &fileName)
+    {
+        std::ifstream file(fileName);
+        for (auto & i : m_matrix)
+        {
+            for (long double & j : i)
+            {
+                file >> j;
+            }
+        }
+    }
+};
+
+void randGenMatrix(Matrix<SIZE_N> &A, int N, int start = 1, int end = 100)
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<CalVar> dist(start, end);
+
+    for (unsigned i = 0; i < N; ++i)
+    {
+        for (unsigned j = 0; j < N; ++j)
+        {
+            A[i][j] = dist(mt);
+        }
+    }
 }
 
+void genMatrixA(Matrix<SIZE_N> &A)
+{
+    for (unsigned i = 0; i < SIZE_N; ++i)
+    {
+        for (unsigned j = 0; j < SIZE_N; ++j)
+        {
+            if (j < SIZE_N - i)
+                A[i][j] = i + 1;
+            else
+                A[i][j] = 0;
+        }
+    }
+}
 
 int main()
 {
-
+    Matrix<SIZE_N> A(SIZE_N, SIZE_N);
+    genMatrixA(A);
+    A.print();
 
     return 0;
 }
