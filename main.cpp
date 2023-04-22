09dc7bce57e4f61d651c0400127ce7c2e762cda9
losimen <koval.pavlo03@gmail.com>
2023-04-22 19:29:13 +0300

    get matrix A

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <array>
+
+constexpr int SIZE_N = 6;
+typedef std::array<std::array<long double, SIZE_N>, SIZE_N> Matrix;
+
+
+Matrix genMatrixA()
+{
+    // 111…111
+    //222…220
+    //333…300
+    //…
+    //n000…00
+
+    Matrix A;
+    for (int i = 0; i < SIZE_N; ++i)
+    {
+        for (int j = 0; j < SIZE_N; ++j)
+        {
+            A[i][j] = i * 10 + j;
+        }
+    }
+
+    return A;
+}
 
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+
 
     return 0;
 }
