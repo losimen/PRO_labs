118e8a729b58d7225438c30635661205bc1b379b
losimen <koval.pavlo03@gmail.com>
2023-05-23 15:18:17 +0300

    mul matrices default

--- main.cpp
+++ main.cpp
@@ -72,23 +72,53 @@ void genMatrix(Matrix &matrix)
     auto &m_matrix = matrix.matrix();
 
     for(int i = 0; i < matrix.rows(); i++)
-    {
         for(int j = 0; j < matrix.cols(); j++)
-        {
             m_matrix[i][j] = randGenNumber();
+}
+
+
+void mulMatrices(Matrix &result, Matrix &matrixA, Matrix &matrixB)
+{
+    auto m_matrixA = matrixA.matrix();
+    auto m_matrixB = matrixB.matrix();
+    auto &m_result = result.matrix();
+
+    for(int i = 0; i < matrixA.rows(); i++)
+    {
+        for(int j = 0; j < matrixB.cols(); j++)
+        {
+            m_result[i][j] = 0;
+            for(int k = 0; k < matrixA.cols(); k++)
+            {
+                m_result[i][j] += m_matrixA[i][k] * m_matrixB[k][j];
+            }
         }
     }
-
 }
 
 
 int main()
 {
-    Matrix matrixA(6, 3);
-    Matrix matrixB(5, 6);
+    Matrix matrixA(10, 5);
+    Matrix matrixB(5, 10);
 
     genMatrix(matrixA);
-    matrixA.print();
+    genMatrix(matrixB);
+
+//
+//    std::cout << "Matrix A:" << std::endl;
+//    matrixA.print();
+//    std::cout << std::endl;
+//
+//    std::cout << "Matrix B:" << std::endl;
+//    matrixB.print();
+//    std::cout << std::endl;
+
+    Matrix result(matrixA.rows(), matrixB.cols());
+    mulMatrices(result, matrixA, matrixB);
+    std::cout << "Result:" << std::endl;
+    result.print();
+    std::cout << std::endl;
 
     return 0;
 }
